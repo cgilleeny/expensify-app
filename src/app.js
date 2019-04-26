6caeb1edab52703dc2cb8cf4e7aafa9fccb2ad8b
Caroline Gilleeny <cgilleeny@live.com>
2019-04-26 10:38:06 -0700

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+// import { addExpense } from './actions/expenses';
+// import { setTextFilter } from './actions/filters';
+// import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 const store = configureStore();
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-})
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 1500 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 1000, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+
+// store.subscribe(() => {
+//   const state = store.getState();
+//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//   console.log(visibleExpenses);
+// })
+// store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 1500 }));
+// store.dispatch(addExpense({ description: 'Gas bill', amount: 1000, createdAt: 1000 }));
+// store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const jsx = (
   <Provider store={store}>
